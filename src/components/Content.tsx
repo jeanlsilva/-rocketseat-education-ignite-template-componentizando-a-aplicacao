9cd37a581a327a92832079a69bcebeb2d1604910
Jean Silva <jeanslucasilva@gmail.com>
2021-03-17 08:37:44 -0300

    componentizacao do sidebar e content de aplicacao

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,39 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  selectedGenre: {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  };
+
+  movies: MovieProps[];
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {props.movies.map(movie => (
+          <MovieCard key={movie.Title} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  );
 }
